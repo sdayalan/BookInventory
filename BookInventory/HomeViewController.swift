8a0a6a3572d42474f516fa33eced4a87eab5759b
Siva Dayalan <sdayalan@gmail.com>
2018-04-14 20:33:24 +0800

    Renamed default `ViewController` into `HomeViewController`

---
+++ BookInventory/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  BookInventory
+//
+//  Created by Siva Dayalan on 14/4/18.
+//  Copyright © 2018 Siva Dayalan. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
