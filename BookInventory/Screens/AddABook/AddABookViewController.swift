63470b765c1ff950a5744a149886ab3a40129847
Siva Dayalan <sdayalan@gmail.com>
2018-04-14 20:44:55 +0800

    Created groups with folder, catered for each screens. Moved `HomeViewController` to `Home`. Added `AddABookViewController` for `Add A Book` view Added `BookListViewController` for `BookList` view

---
+++ BookInventory/Screens/AddABook/AddABookViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddABookViewController.swift
+//  BookInventory
+//
+//  Created by Siva Dayalan on 14/4/18.
+//  Copyright © 2018 Siva Dayalan. All rights reserved.
+//
+
+import UIKit
+
+class AddABookViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
